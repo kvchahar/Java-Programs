fe3fc3f8695ef251dc508d45550e6da3ad637170
kvchahar <68537060+kvchahar@users.noreply.github.com>
2020-08-23 14:36:32 +0530

    Add files via upload

---
+++ VariablesAndNames.java
@@ -0,0 +1,40 @@
+package variables;
+
+public class VariablesAndNames {
+    public static void main(String[] args) {
+        String course1 = "English";
+        String course2 = "DBMS";
+        String course3 = "OOPS";
+        String course4 = "OS";
+        String course5 = "SE";
+        String course6 = "Aptitude";
+        String course7 = "MathS";
+        String course8 = "Ethics";
+        String teacher1 = "Atul Sir";
+        String teacher2 = "Rishi Sir";
+        String teacher3 = "Divyansh Sir";
+        String teacher4 = "Hitendra Sir";
+        String teacher5 = "Asheesh Sir";
+        String teacher6 = "Jay Prakash Sir";
+        String teacher7 = "Himanshu Sir";
+        String teacher8 = "Nirbhaya Sir";
+        System.out.printf("%-26s\t%-15s", "SUBJECTS", "FACULTY NAMES");
+        System.out.println();
+        System.out.printf("%-26s\t%-15s", course1, teacher1);
+        System.out.println();
+        System.out.printf("%-26s\t%-15s", course2, teacher2);
+        System.out.println();
+        System.out.printf("%-26s\t%-15s", course3, teacher3);
+        System.out.println();
+        System.out.printf("%-26s\t%-15s", course4, teacher4);
+        System.out.println();
+        System.out.printf("%-26s\t%-15s", course5, teacher5);
+        System.out.println();
+        System.out.printf("%-26s\t%-15s", course6, teacher6);
+        System.out.println();
+        System.out.printf("%-26s\t%-15s", course7, teacher7);
+        System.out.println();
+        System.out.printf("%-26s\t%-15s", course8, teacher8);
+
+    }
+}
